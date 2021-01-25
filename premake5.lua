74f13f1a56e6587b00ae9d903ac374693c4e7dd0
unbellum <unbellum@yahoo.com>
2021-01-24 21:38:42 -0500

    Basic build setup for rvv engine

--- premake5.lua
+++ premake5.lua
@@ -12,11 +12,12 @@ project "ImGui"
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-    "imgui_demo.cpp"
+        "imgui_demo.cpp"
 	}
 
 
